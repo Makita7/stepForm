478783f1e42c52baef898a5f334715e5b520ec87
sofia-makita <smakita@iadb.org>
2024-03-10 15:54:49 -0300

    summary elements

--- src/pages/summaryPage.vue
+++ src/pages/summaryPage.vue
@@ -7,25 +7,31 @@
     <p class="detail mb-8">Double-check everything looks OK before confirming.</p>
 
     <v-card class="sumC elevation-0 roundm" width="90%">
-        <div class="d-flex align-center">
+        <div class="d-flex align-center h pt-4">
             <v-col>
-                <p class="titles">Arcade (Monthly)</p>
+                <p class="titles plan">Arcade (Monthly)</p>
                 <router-link to="/" class="detail">Change</router-link>
             </v-col>
             <v-spacer/>
-            <p class="pDetail">$9/mo</p>
+            <p class="pDetail mr-6 font-weight-bold plan">$9/mo</p>
         </div>
+        <v-divider class="mx-8 mt-4"/>
         <!-- TODO: this element should have a v-for for each element in add ons -->
-        <div class="d-flex align-center pl-3">
+        <div class="d-flex align-center pl-3 h">
             <p class="detail">Online service</p>
             <v-spacer/>
-            <p class="pDetail">$1/mo</p>
+            <p class="pDetail mr-6">$1/mo</p>
         </div>
     </v-card>
-    <div class="d-flex total align-center">
+    <div class="d-flex total align-center h pl-3">
         <p class="detail">Total (per month)</p>
         <v-spacer/>
-        <p class="">+$12/mo</p>
+        <p class="title mr-6">+$12/mo</p>
+    </div>
+
+    <div class="d-flex mx-2">
+        <v-btn class="align b l" variant="text">Go Back</v-btn>
+        <v-btn class="align elevation-0 b r w" color="var(--purplish-blue)" size="large">Confirm</v-btn>
     </div>
 </template>
 
@@ -33,10 +39,32 @@
     .sumC{
         background-color: var(--selected-blue);
         margin-left: 5%;
-        min-height: 100%;
+        .pDetail{
+            color: var(--marineblue);
+        }
+        .plan{
+            font-size: 1.1rem;
+        }
+    }
+    .h{
+        height: 4rem;
     }
     .total{
         width: 90%;
         margin-left: 5%;
+        .title{
+            color: var(--purplish-blue);
+            font-size: 1.5rem;
+            font-weight: bold;
+        }
+    }
+
+    :deep(){
+        .w{
+            color: white;
+            span{
+                font-weight: bolder !important;
+            }
+        }
     }
 </style>
